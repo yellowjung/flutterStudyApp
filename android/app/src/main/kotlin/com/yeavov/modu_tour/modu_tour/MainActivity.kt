939f8f335269c95c41f03a264de2dda4f5cdbffc
YoungdonJung <yeavov@gmail.com>
2022-01-08 15:39:30 +0900

    init flutter app example

---
+++ android/app/src/main/kotlin/com/yeavov/modu_tour/modu_tour/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yeavov.modu_tour.modu_tour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/signPage.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:modu_tour/data/user.dart';
+
+class SignPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SignPage();
+}
+
+class _SignPage extends State<SignPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
